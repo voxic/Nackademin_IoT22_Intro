0244eca840a31c0e529d2ef431a53a002ea50f11
Emil <nilsson.emi@gmail.com>
2022-09-07 12:35:53 +0200

    added python chatt

---
+++ PythonChatt/main.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import paho.mqtt.client as paho
+import json
+
+
+running = True
+configDone = False
+connected = False
+
+
+name = input('What is your name? : ')
+topic = input('Enter a chatroom : ')
+
+
+def on_message(client, userdata, msg):
+    messageObj = json.loads(str(msg.payload.decode("utf-8")))
+    if(messageObj["name"] != name):
+        print("\n" + messageObj["name"] + " said: " + messageObj["message"])
+
+def on_connect(client, userdata, flags, rc):
+    client.subscribe("nackademinchat/"+topic)
+
+# Connect to MQTT and report in
+client = paho.Client(name,True, None, paho.MQTTv31)
+client.will_set("nackademinchat/"+topic, payload=json.dumps({"name": name, "message" : "Disconnected"}), qos=0, retain=False)
+client.connect("test.mosquitto.org", 1883)
+client.on_message = on_message 
+client.on_connect = on_connect
+client.loop_start()
+
+
+
+while True:
+    message = input()
+    tempMess = {"name" : name, "message":message}
+    client.publish("nackademinchat/"+topic, json.dumps(tempMess), qos=1)
+
+
+
